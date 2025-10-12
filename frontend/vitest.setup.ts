5a8e28837aa964eee24435b57ffc53083c0763b8
Krizo <krizzb@gmail.com>
2025-10-12 23:42:56 +0200

    Added import

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -1,5 +1,7 @@
 import { expect } from 'vitest';
 import * as matchers from '@testing-library/jest-dom/matchers';
+import '@testing-library/jest-dom';
+
 
 // Extend Vitest's expect with jest-dom matchers
 expect.extend(matchers);
